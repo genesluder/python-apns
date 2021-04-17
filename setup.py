f929c5f2cd9a58232a7e4ec3cad6a49a7cd8036f
Gene Sluder <me@genesluder.com>
2021-04-17 02:24:51 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     'gobiko',
     'gobiko.apns'
   ],
-  version = '0.1.5',
+  version = '0.1.6',
   description = 'A library for interacting with APNs using HTTP/2 and token-based authentication.',
   author = 'Gene Sluder',
   author_email = 'gene@gobiko.com',
